5b7a3f2b2a36df866235a26967b23bb2582250ad
nishantsinghraghuvanshi <singhnishant0131@gmail.com>
2024-03-18 22:49:56 +0530

    FizzBuzz and Rock Paper Scissors Project

---
+++ Stone.js
@@ -0,0 +1,36 @@
+let result_final = "";
+function winner(me,computer){
+    if (me == "Rock" & computer == "Scissors"){
+        result_final = "You Win !";
+    }
+    else if (me == "Rock" & computer == "Paper"){
+        result_final = "You Win !";
+    }
+    else if (me == "Paper" & computer == "Scissors"){
+        result_final = "You Lose !";
+    }
+    else if (me == "Paper" & computer == "Rock"){
+        result_final = "You Win !";
+    }
+    else if (me == "Scissors" & computer == "Paper"){
+        result_final = "You Win !";
+    }
+    else if (me == "Scissors" & computer == "Rock"){
+        result_final = "You Lose !";
+    }
+    else if(me == computer){
+        result_final = "Draw";
+    }
+    else{
+        result_final = "Invalid";
+    }
+}
+function game(word){
+    let num = Math.floor(Math.random()*3)
+    const all = ["Rock","Paper","Scissors"]
+    comp_out = all[num]
+    winner(word,comp_out)
+    console.log(result_final)
+}
+const text_w = document.getElementById("result");
+text_w.textContent = `Result : ${result_final}`;
